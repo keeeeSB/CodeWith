32dfe5243f44929ac2b3c663191fb7eb4c4a78d1
KeeeeSB <kfp.a@icloud.com>
2025-11-27 10:21:26 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+  }
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'posts#index'
